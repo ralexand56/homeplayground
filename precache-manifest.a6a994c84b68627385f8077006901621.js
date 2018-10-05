1e0b1235ca741b571d9468baf64a95adc16fd7e9
Rico Alexander <ralexand56@live.com>
2018-10-05 09:57:54 +0300

    Updates

---
+++ precache-manifest.a6a994c84b68627385f8077006901621.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dc5b3d8450c73f1013c",
+    "url": "/static/js/main.5dc5b3d8.chunk.js"
+  },
+  {
+    "revision": "41e70e0bbea67455f7ba",
+    "url": "/static/js/1.41e70e0b.chunk.js"
+  },
+  {
+    "revision": "5dc5b3d8450c73f1013c",
+    "url": "/static/css/main.1dc231b0.chunk.css"
+  },
+  {
+    "revision": "654d5c04c827bab0822dcd88bec36d27",
+    "url": "/index.html"
+  }
+];
